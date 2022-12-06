419ecd0d0c7dda189e553b5ae95b3fb55846dadc
Khawlah <khawlah_alrashed@hotmail.com>
2022-12-06 12:27:12 +0300

    Add MVVM Design Pattern

---
+++ ExpDate/Model/ProductModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductModel.swift
+//  ExpDate
+//
+//  Created by Khawlah on 06/12/2022.
+//
+
+import Foundation
---
+++ ExpDate/ViewModel/ProductViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductViewModel.swift
+//  ExpDate
+//
+//  Created by Khawlah on 06/12/2022.
+//
+
+import Foundation
