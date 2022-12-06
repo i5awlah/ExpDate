1d3a7137d06a8def92acc1f65d54d2145cd70f58
Khawlah <khawlah_alrashed@hotmail.com>
2022-12-06 12:21:00 +0300

    Add Color extension

---
+++ ExpDate/Utilities/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  ExpDate
+//
+//  Created by Khawlah on 06/12/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let greenProgress = Color("greenProgress")
+    static let redProgress = Color("redProgress")
+    static let yellowProgress = Color("yellowProgress")
+}
