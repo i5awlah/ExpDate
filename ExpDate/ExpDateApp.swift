6a88c44d8904ccfef581f6327dde21cb28b7b9ac
Khawlah <khawlah_alrashed@hotmail.com>
2022-12-06 10:40:48 +0300

    Initial Commit

---
+++ ExpDate/ExpDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpDateApp.swift
+//  ExpDate
+//
+//  Created by Khawlah on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
